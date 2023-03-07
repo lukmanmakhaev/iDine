4db059cbcfedef5a6399a298f3ee08cb6d4e3a30
Lukman <lukmanmakhaev@gmail.com>
2023-03-08 00:52:40 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Lukman Makhaev on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
